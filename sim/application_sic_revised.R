85dc5f3e8fc7d9bddfc45b25c49a8d3ba2ca2b5a
myeongjong <kmj.stat@gmail.com>
2023-04-02 17:32:56 -0500

    [Sim] Consider the Shapiro-Botha method for real-data application

---
+++ sim/application_sic_revised.R
@@ -0,0 +1,164 @@
+###########################################################################
+###
+###    Author : MYEONGJONG KANG
+###    E-mail : kmj.stat@gmail.com
+###
+###    Description : Real data application
+###
+###########################################################################
+
+library(geoR)
+library(NPcov)
+library(npsp)
+
+rm(list = ls())
+set.seed(11072022) # set.seed(11032022)
+
+###########################################################################
+### data
+###########################################################################
+
+data("SIC", package = "geoR")
+rm(sic.some, sic.367)
+
+extract_sic <- function(target, xmax = 8)
+{
+  dmat          <- dist(sic.100$coords)
+  dvec          <- as.vector(dmat)
+  dmat          <- as.matrix(dmat)
+
+  vals          <- rep(NA, length(dvec))
+  for(i in 1:length(vals)) {
+
+    row           <- which(dmat == dvec[i], arr.ind = TRUE)[1,1]
+    col           <- which(dmat == dvec[i], arr.ind = TRUE)[1,2]
+    average       <- mean(target$data)
+    vals[i]       <- (target$data[row] - average) * (target$data[col] - average)
+  }
+
+  vhat          <- var(target$data) * (length(target$data)-1) / length(target$data)
+  y_err         <- vals[order(dvec)] # / ( var(target$data) * (length(target$data)-1) / length(target$data) )
+  x             <- dvec[order(dvec)]
+  xmax0         <- max(x)
+  x             <- x / xmax0 * xmax
+
+  return( list(r = x, xmax0 = xmax0, xmax = xmax, chat = y_err, vhat = vhat) )
+}
+
+output_sic100 <- extract_sic(target = sic.100)
+output_sicall <- extract_sic(target = sic.all)
+
+###########################################################################
+### Shapiro-Botha
+###########################################################################
+
+svar_bin      <- svariso(sic.100$coords, sic.100$data)
+svar_h        <- h.cv(svar_bin)$h
+svar_np       <- np.svar(svar_bin, h = svar_h)
+svm           <- fitsvar.sb.iso(svar_np)
+sbout         <- list(svar_bin = svar_bin, svar_h = svar_h, svar_np = svar_np, svm = svm)
+
+###########################################################################
+### setting (our method)
+###########################################################################
+
+expand        <- FALSE
+eval          <- NULL
+k             <- 5
+kernel        <- "gaussian"
+# shape         <- "general"
+method        <- "reflection"
+dist_init     <- "exponential"
+min_iter      <- 20
+max_iter      <- 200
+tol           <- 1e-3
+check         <- 5
+ncores        <- NULL
+
+###########################################################################
+### isotropic estimator
+###########################################################################
+
+ms            <- c(5, 6, 7, 8)
+hs            <- seq(0.15, 0.2, by = 0.01)
+inputs        <- expand.grid(ms, hs) ; colnames(inputs) <- c("ms", "hs")
+inputs$taus   <- rep(0.1, nrow(inputs))
+inputs$ns     <- inputs$ms * 10
+
+ptm_cv_iso    <- proc.time()
+cvout_iso     <- cv_pdnr(x = output_sic100$r, y = output_sic100$chat / output_sic100$vhat, hs = inputs$hs, ns = inputs$ns, ms = inputs$ms, taus = inputs$taus, expand = expand, eval = eval, k = k, kernel = kernel, shape = "general", method = method, dist_init = dist_init, min_iter = min_iter, max_iter = max_iter, tol = tol, check = check, ncores = ncores)
+ptm_cv_iso    <- proc.time() - ptm_cv_iso
+ptm_cv_iso # 15515.80
+
+ptm_best_iso  <- proc.time()
+vhat          <- output_sic100$vhat
+for(i in 1:100) {
+
+  message("Step ", i)
+
+  vhat_old      <- vhat
+  bestout_iso   <- pdnr(x = output_sic100$r, y = output_sic100$chat / vhat, h = cvout_iso$input.best$h, n = cvout_iso$input.best$n, m = cvout_iso$input.best$m, tau = cvout_iso$input.best$tau, eval = eval, kernel = kernel, shape = "general", method = method, dist_init = dist_init, min_iter = min_iter, max_iter = max_iter, tol = tol, check = check)
+
+  vgrid         <- seq(from = max(0, vhat - 1500), to = vhat + 1500, length.out = 1000)
+  msevec        <- c()
+  for(j in 1:length(vgrid)) msevec[j] <- mean((output_sic100$chat - vgrid[j] * bestout_iso$yhat)^2)
+
+  vhat          <- vgrid[which.min(msevec)]
+
+  if(abs(vhat_old - vhat) < 0.005 * output_sic100$vhat) break
+}
+ptm_best_iso  <- proc.time() - ptm_best_iso
+i ; ptm_best_iso # 2302.58
+
+vhat_iso      <- vhat
+
+###########################################################################
+### monotone estimator
+###########################################################################
+
+ms            <- c(2, 3, 4, 5)
+hs            <- seq(0.01, 0.1, by = 0.01)
+inputs        <- expand.grid(ms, hs) ; colnames(inputs) <- c("ms", "hs")
+inputs$taus   <- rep(0.1, nrow(inputs))
+inputs$ns     <- inputs$ms * 10
+
+ptm_cv_mon    <- proc.time()
+cvout_mon     <- cv_pdnr(x = output_sic100$r, y = output_sic100$chat / output_sic100$vhat, hs = inputs$hs, ns = inputs$ns, ms = inputs$ms, taus = inputs$taus, expand = expand, eval = eval, k = k, kernel = kernel, shape = "monotone", method = method, dist_init = dist_init, min_iter = min_iter, max_iter = max_iter, tol = tol, check = check, ncores = ncores)
+ptm_cv_mon    <- proc.time() - ptm_cv_mon
+ptm_cv_mon # 283.28
+
+ptm_best_mon  <- proc.time()
+vhat          <- output_sic100$vhat
+for(i in 1:100) {
+
+  message("Step ", i)
+
+  vhat_old      <- vhat
+  bestout_mon   <- pdnr(x = output_sic100$r, y = output_sic100$chat / vhat, h = cvout_mon$input.best$h, n = cvout_mon$input.best$n, m = cvout_mon$input.best$m, tau = cvout_mon$input.best$tau, eval = eval, kernel = kernel, shape = "monotone", method = method, dist_init = dist_init, min_iter = min_iter, max_iter = max_iter, tol = tol, check = check)
+
+  vgrid         <- seq(from = max(0, vhat - 1500), to = vhat + 1500, length.out = 1000)
+  msevec        <- c()
+  for(j in 1:length(vgrid)) msevec[j] <- mean((output_sic100$chat - vgrid[j] * bestout_mon$yhat)^2)
+
+  vhat          <- vgrid[which.min(msevec)]
+
+  if(abs(vhat_old - vhat) < 0.005 * output_sic100$vhat) break
+}
+ptm_best_mon  <- proc.time() - ptm_best_mon
+i ; ptm_best_mon # 58.56
+
+vhat_mon      <- vhat
+
+###########################################################################
+### save
+###########################################################################
+
+plot(output_sic100$r, output_sic100$chat, xlab = "r", ylab = "c", col = "gray", ylim = c(-1.25, 1.25) * output_sic100$vhat)
+abline(h = 0, lwd = 2, col = "black", lty = "dashed")
+lines(bestout_iso$eval, bestout_iso$yhat * vhat_iso, lwd = 2, col = "red")
+lines(bestout_mon$eval, bestout_mon$yhat * vhat_mon, lwd = 2, col = "blue")
+lines(svm$fit$u * output_sic100$xmax / output_sic100$xmax0, svm$sill - svm$fit$fitted.sv, type = "o", col = "green", lwd = 2)
+
+save(sic.100, sic.borders, output_sic100, sbout, cvout_iso, ptm_cv_iso, bestout_iso, vhat_iso, ptm_best_iso, cvout_mon, ptm_cv_mon, bestout_mon, vhat_mon, ptm_best_mon, file = "out/appout_sic100_gaussian.RData")
+
+
---
+++ sim/visualization_application_sic_revised.R
@@ -0,0 +1,70 @@
+###########################################################################
+###
+###    Author : MYEONGJONG KANG
+###    E-mail : kmj.stat@gmail.com
+###
+###    Description : Visualization
+###
+###########################################################################
+
+library(geoR)
+library(sf)
+library(ggplot2)
+library(gridExtra)
+library(grid)
+
+###########################################################################
+###
+###########################################################################
+
+data(SIC)
+
+p1          <- ggplot() +
+  geom_sf(data = st_linestring(sic.borders), lwd = 2, col = "grey20") +
+  geom_sf(data = st_as_sf(data.frame(x = sic.100$coords[, "V2"],
+                                     y = sic.100$coords[, "V3"],
+                                     z = sic.100$data),
+                          coords = c("x", "y")),
+          pch = 21, aes(size = z), fill = alpha("#377EB8", 0.7), col = "grey20") +
+  theme_bw() + xlab("coordinate 1") + ylab("coordinate 2") +
+  guides(size = guide_legend(title="(1/10th of a mm)", title.position = "right", nrow = 2, byrow = TRUE)) +
+  theme(legend.text = element_text(size = 8), legend.title = element_text(size = 8), legend.direction = 'horizontal', legend.position = "top", legend.spacing.x = unit(0.1, 'cm'))
+
+###########################################################################
+###
+###########################################################################
+
+load(file = "out/appout_sic100_gaussian.RData")
+
+visdat <- data.frame(type = c(rep("Our isotropic", length(bestout_iso$eval)),
+                              rep("Our monotone", length(bestout_mon$eval)),
+                              rep("Shapiro-Botha", length(sbout$svm$fit$u))),
+                     x = c(bestout_iso$eval / output_sic100$xmax * output_sic100$xmax0,
+                           bestout_mon$eval / output_sic100$xmax * output_sic100$xmax0,
+                           sbout$svm$fit$u),
+                     y = c(bestout_iso$yhat * vhat_iso,
+                           bestout_mon$yhat * vhat_mon,
+                           sbout$svm$sill - sbout$svm$fit$fitted.sv))
+
+formatting <- function(l) {
+
+  l <- format(l, scientific = TRUE) # turn in to character string in scientific notation
+  # l <- gsub("^(.*)e", "'\\1'e", l) # quote the part before the exponent to keep all the digits
+  # l <- gsub("e", "%*%10^", l) # turn the 'e+' into plotmath format
+  # parse(text = l) # return this as an expression
+}
+
+p3 <- ggplot() +
+  geom_point(data = data.frame(x = output_sic100$r / output_sic100$xmax * output_sic100$xmax0, y = output_sic100$chat), aes(x = x, y = y), color = "gray75") +
+  geom_line(data = visdat, aes(x = x, y = y, col = type, lty = type), lwd = 1.2, alpha = 0.9) +
+  geom_hline(yintercept = 0, linetype="solid", color = "black") +
+  scale_color_manual(name = NULL, labels = c("Our isotropic" = "Our isotropic", "Our monotone" = "Our monotone", "Shapiro-Botha" = "Shapiro-Botha"), values = c("Our isotropic" = "#E41A1C", "Our monotone" = "#4DAF4A", "Shapiro-Botha" = "#377EB8")) +
+  scale_linetype_manual(name = NULL, labels = c("Our isotropic" = "Our isotropic", "Our monotone" = "Our monotone", "Shapiro-Botha" = "Shapiro-Botha"), values = c("Our isotropic" = "twodash", "Our monotone" = "twodash", "Shapiro-Botha" = "dashed")) +
+  # scale_y_continuous(labels = formatting) +
+  theme_bw() + xlab("distance") + ylab("covariance") +
+  coord_cartesian(ylim = c(-0.25, 1.05) * output_sic100$vhat) +
+  guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
+  theme(legend.direction = 'horizontal', legend.key.width = unit(1.5, "cm"), legend.position = "top", legend.justification = "right")
+
+p.merge       <- grid.arrange(p1, p3, nrow = 1)
+ggplot2::ggsave("img/application_sic100.pdf", p.merge, width = 7.5, height = 3.5)
